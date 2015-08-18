1f3f7ac715882b745b529a120f314c66e8a99926
ganapati <justinjohn107@gmail.com>
2015-08-17 21:04:56 -0400

    adding conditional to see if we are in two pane mode

--- app/src/main/java/com/justinvarghesejohn/spotifystreamer/SearchActivity.java
+++ app/src/main/java/com/justinvarghesejohn/spotifystreamer/SearchActivity.java
@@ -8,6 +8,9 @@ import android.view.MenuItem;
 
 public class SearchActivity extends AppCompatActivity {
 
+    private static final String DETAILFRAGMENT_TAG = "DFTAG";
+    private boolean mTwoPane;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +21,18 @@ public class SearchActivity extends AppCompatActivity {
 //                    .add(R.id.fragment_search, new SearchFragment())
 //                    .commit();
 //        }
+        if (findViewById(R.id.tracks_detail_container) != null) {
+            // tracks detail container is present which means we are in two pane mode
+            mTwoPane = true;
+
+            if (savedInstanceState == null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.tracks_detail_container, new TracksFragment())
+                        .commit();
+            }
+        } else {
+            mTwoPane = false;
+        }
     }
 
 
